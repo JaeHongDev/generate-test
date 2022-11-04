4ba6e0eeb9cc1f123a0dad92b394e9487485053c
jaehong <println@kakao.com>
2022-11-04 17:28:20 +0900

    web init

---
+++ generate-test-web/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    1234
+    <EditorContent/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import StarterKit from '@tiptap/starter-kit'
+import { Editor, EditorContent } from '@tiptap/vue-2'
+export default Vue.extend({
+  name: 'App',
+
+  data: () => ({
+    editor: new Editor({
+      extensions:[StarterKit],
+    }),
+  }),
+});
+</script>
